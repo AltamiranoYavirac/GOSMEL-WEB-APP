99efef8f655946f6d5cc5ff9e7a193cb3149bf3e
cdbjimenez <cdb.jimenez@yavirac.edu.ec>
2026-08-29 17:37:23 -0500

    change envs

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    NEXT_PRODUCTION_SUPABASE_URL: process.env.NEXT_PRODUCTION_SUPABASE_URL,
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    NEXT_PRODUCTION_SUPABASE_PUBLISHABLE_KEY:
-      process.env.NEXT_PRODUCTION_SUPABASE_PUBLISHABLE_KEY,
-    SUPABASE_PUBLISHABLE_KEY: process.env.SUPABASE_PUBLISHABLE_KEY,
-  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
